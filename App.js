6a1807f33ad113822e0ae4849efb2615624eff14
tergelm <tergel2923@gmail.com>
2022-07-07 01:29:36 -0500

    initial commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00f',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyle: {
+    color: '#fff',
+    fontSize: 32,
+  }
+});
